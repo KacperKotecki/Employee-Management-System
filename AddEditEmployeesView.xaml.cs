aba9369c25e6d6016efdd493f5c695961a4a6c3d
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-20 13:55:29 +0200

    Add AddEditEmployeesView

---
+++ AddEditEmployeesView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Employee_Management_System
+{
+    /// <summary>
+    /// Interaction logic for AddEditEmployeesView.xaml
+    /// </summary>
+    public partial class AddEditEmployeesView : Window
+    {
+        public AddEditEmployeesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
