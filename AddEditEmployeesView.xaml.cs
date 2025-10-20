09fd650614f49bfb19156aed1cb7d3265f078689
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-20 17:28:51 +0200

    Introduce Mahapps freamwork

--- AddEditEmployeesView.xaml.cs
+++ AddEditEmployeesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Employee_Management_System
     /// <summary>
     /// Interaction logic for AddEditEmployeesView.xaml
     /// </summary>
-    public partial class AddEditEmployeesView : Window
+    public partial class AddEditEmployeesView : MetroWindow
     {
         public AddEditEmployeesView()
         {
