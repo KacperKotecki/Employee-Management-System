e4bcf6ef60f8301f631331ce95e668859184624d
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-28 22:02:53 +0100

    feat: Add global exception handler

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace Employee_Management_System
 {
@@ -13,5 +15,16 @@ namespace Employee_Management_System
     /// </summary>
     public partial class App : Application
     {
+        private async void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var metroWindow = Application.Current.Windows.OfType<MetroWindow>().FirstOrDefault(x => x.IsActive);
+            if (metroWindow == null) return; // Zabezpieczenie, jeśli żadne okno nie jest aktywne
+
+            var result = await metroWindow.ShowMessageAsync("Nieoczekiwany wyjątek",
+                $"Wystąpił nioczekiwany wyjątek {e.Exception.Message}",
+                MessageDialogStyle.Affirmative);
+
+            e.Handled = true;
+        }
     }
 }
