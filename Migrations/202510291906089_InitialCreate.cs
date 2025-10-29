4541eb69e805139a3abedf55a703469444446352
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-29 20:32:45 +0100

    feat: Enable database migration in Entity Framework
    This commit introduces the initial database schema, generated by Entity Framework Migrations.
    After enabling migrations, the add-migration InitialCreate command was run, which inspected the domain models and Fluent API configurations to produce the following database structure:
    
        Departments Table: Created to store department information (Id, Name).
    
        Positions Table: Created to store job position information (Id, Name).
    
        Employees Table: Created as the central table, containing employee details.
    
    Relationships:
    
    •	A one-to-many relationship was established between Departments and Employees using the DepartmentId foreign key.
    •	A one-to-many relationship was established between Positions and Employees using the PositionId foreign key.

---
+++ Migrations/202510291906089_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿namespace Employee_Management_System.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 30),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 20),
+                        LastName = c.String(nullable: false, maxLength: 20),
+                        Email = c.String(nullable: false, maxLength: 30),
+                        Phone = c.String(maxLength: 15),
+                        Salary = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        HireDate = c.DateTime(),
+                        DismissalDate = c.DateTime(),
+                        DepartmentId = c.Int(nullable: false),
+                        PositionId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Departments", t => t.DepartmentId, cascadeDelete: true)
+                .ForeignKey("dbo.Positions", t => t.PositionId, cascadeDelete: true)
+                .Index(t => t.DepartmentId)
+                .Index(t => t.PositionId);
+            
+            CreateTable(
+                "dbo.Positions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 30),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employees", "PositionId", "dbo.Positions");
+            DropForeignKey("dbo.Employees", "DepartmentId", "dbo.Departments");
+            DropIndex("dbo.Employees", new[] { "PositionId" });
+            DropIndex("dbo.Employees", new[] { "DepartmentId" });
+            DropTable("dbo.Positions");
+            DropTable("dbo.Employees");
+            DropTable("dbo.Departments");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Employee_Management_System.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Employee_Management_System.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Employee_Management_System.ApplicationDbContext";
+        }
+
+        protected override void Seed(Employee_Management_System.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
